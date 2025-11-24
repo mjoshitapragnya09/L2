5f9ce32e0141369ff82949ec017566fa8f603d5b
mjoshitapragnya09 <m.joshitap09@gmail.com>
2025-11-24 22:59:26 +0530

    Create templateShorthand.js

---
+++ templateShorthand.js
@@ -0,0 +1,13 @@
+const username = "Sam";
+const course = "JavaScript";
+console.log(`Hello ${username}, welcome to the ${course} course!`);
+const name = "Sam";
+const age = 21;
+const student = {
+  name,
+  age,
+  greet() {
+    console.log("Hello");
+  }
+};
+const getFullName = (first, last) => `${first} ${last}`;
