298e30b6a76321ab6c0d937c4e9423499d1b0a21
mjoshitapragnya09 <m.joshitap09@gmail.com>
2025-11-26 22:43:15 +0530

    Add age management functions in age.js

---
+++ age.js
@@ -0,0 +1,11 @@
+let age = 20;
+function displayAge() {
+  console.log("Current age:", age);
+}
+function changeAge() {
+  age = 25;
+  console.log("Age updated inside function to:", age);
+}
+displayAge();  
+changeAge();    
+displayAge();   
